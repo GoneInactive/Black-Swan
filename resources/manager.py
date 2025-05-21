c2e59c2cd407d40278bff26dd142639a5aed540f
Ryan Watts <watts.ryan03@gmail.com>
2025-05-21 17:36:50 -0600

    feat: create resource manager
    - Added manager.py in resources directory
    - Added image loader in resources/images
    - Removed "plots/ from .gitignore

---
+++ resources/manager.py
@@ -0,0 +1,23 @@
+def load_resources():
+    """
+    Loads all resources from the resources directory.
+    """
+    pass
+
+def save_resources():
+    """
+    Saves all resources to the resources directory.
+    """
+    pass
+
+def get_resource(resource_name):
+    """
+    Gets a resource from the resources directory.
+    """
+    pass
+
+def update_resource(resource_name, resource):
+    """
+    Updates a resource in the resources directory.
+    """
+    pass
