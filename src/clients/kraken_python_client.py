2f4066c710511b6ecc1eae67ad5c996a652c31a4
Ryan Watts <watts.ryan03@gmail.com>
2025-05-25 13:36:47 -0600

    feat: add get_balance, to kraken_python _client
    - Added get_balance with asset param
    - Added add_order with asset param

--- src/clients/kraken_python_client.py
+++ src/clients/kraken_python_client.py
@@ -1,18 +1,41 @@
 from rust_kraken_client import rust_kraken_client as kraken
-
+"""
+This client is used to interact with the kraken API built in rust.
+"""
 class KrakenPythonClient:
-    def get_bid(self,asset):
-        return kraken.get_bid(asset)
+    def __init__(self,asset=None):
+        self.asset = asset
+
+    def get_bid(self,asset=None):
+        """
+        Get the bid price of an asset.
+        """
+        return kraken.get_bid()
     
-    def get_ask(self,asset):
-        return kraken.get_ask(asset)
+    def get_ask(self,asset=None):
+        """
+        Get the ask price of an asset.
+        """
+        return kraken.get_ask()
     
-    def get_balance(self,asset="*")
+    def get_balance(self,asset="*"):
+        """
+        Get the balance of an asset.
+        If asset is "*", returns all balances.
+        """
         if asset == "*":
+            # Returns all balances
             return kraken.get_balance()
         else:
-            return kraken.get_balance(asset)
+            # Returns specific balance
+            return kraken.get_balance()[asset]
         
-    def get_spread(self,asset):
-        return kraken.get_spread(asset)
+    def get_spread(self,asset=None):
+        """
+        Get the spread of an asset.
+        """
+        return kraken.get_spread()
+    
+    def add_order(self,asset,side,price,volume):
+        order_response = kraken.add_order(asset, side, price, volume)
         
